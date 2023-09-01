b1a4bac8d57bf1cae27bf400c1790c8b3e8b9149
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-09-01 17:48:56 +0300

    Update song.py

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,37 @@
-from config import CONN, CURSOR
+from lib.config import CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        # Commit the changes to the database
+        CURSOR.connection.commit()
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.album))
+        # Get the last inserted row ID and assign it to the instance
+        self.id = CURSOR.lastrowid
+        # Commit the changes to the database
+        CURSOR.connection.commit()
+
+    @classmethod
+    def create(cls, name, album):
+        song = cls(name, album)
+        song.save()
+        return song
